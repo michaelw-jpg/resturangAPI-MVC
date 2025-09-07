7b8564341f946f162e5345d70f1b934043612864
michael.wortel@chasacademy.se <michael.wortel@chasacademy.se>
2025-09-07 23:35:24 +0200

    Add project files.

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using resturangAPI_MVC.Models;
+
+namespace resturangAPI_MVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("api");
+        }
+
+
+        // GET: MenuController
+        public async Task <ActionResult> Index()
+        {
+            var response = _httpClient.GetAsync("api/menus");
+            var menuList = await response.Result.Content.ReadFromJsonAsync<List<Menu>>();
+            return View(menuList);
+        }
+
+        // GET: MenuController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MenuController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MenuController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MenuController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MenuController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MenuController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MenuController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
