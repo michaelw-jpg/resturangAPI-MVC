5388267363c9f76ed141b355f4f8d4cfb12cbefc
Michael Wortel <michael.wortel@chasacademy.se>
2025-09-23 14:48:39 +0200

    fixes to bookings per table

--- ViewModel/Table/PatchTableVM.cs
+++ ViewModel/Table/PatchTableVM.cs
@@ -1,8 +1,12 @@
-﻿namespace resturangAPI_MVC.ViewModel.Table
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace resturangAPI_MVC.ViewModel.Table
 {
     public class PatchTableVM
     {
+        [Display(Name = "Bords Nummer")]
         public int? TableNumber { get; set; }
+        [Display(Name = "Antal Platser")]
         public int? Seats { get; set; }
     }
 }
