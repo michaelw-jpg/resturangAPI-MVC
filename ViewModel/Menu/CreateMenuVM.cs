e7e9d79eb35b5efdc8717be293b7f3bf074a704c
Michael Wortel <michael.wortel@chasacademy.se>
2025-09-24 11:00:13 +0200

    fix displaynames

--- ViewModel/Menu/CreateMenuVM.cs
+++ ViewModel/Menu/CreateMenuVM.cs
@@ -6,19 +6,22 @@ namespace resturangAPI_MVC.ViewModel.Menu
     {
         [Required]
         [StringLength(50)]
+        [Display(Name = "Namn")]
         public string Name { get; set; }
 
-
+        [Display(Name = "Pris")]
         public int Price { get; set; }
 
         [Required]
         [StringLength(200)]
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
 
-
+        [Display(Name = "Populär")]
         public bool IsPopular { get; set; }
 
-        [StringLength(128)]
+        [StringLength(2048)]
+        [Display(Name = "Bild URL")]
         public string? ImageUrl { get; set; }
     }
 }
--- ViewModel/Menu/PatchMenuVM.cs
+++ ViewModel/Menu/PatchMenuVM.cs
@@ -7,18 +7,21 @@ namespace resturangAPI_MVC.ViewModel.Menu
         public int? MenuId { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Namn")]
         public string? Name { get; set; }
 
-        
+        [Display(Name = "Pris")]
         public int? Price { get; set; }
 
         [StringLength(200)]
+        [Display(Name = "Beskrivning")]
         public string? Description { get; set; }
 
-       
+        [Display(Name = "Populär")]
         public bool IsPopular { get; set; }
 
-        [StringLength(128)]
+        [StringLength(2048)]
+        [Display(Name = "Bild URL")]
         public string? ImageUrl { get; set; }
     }
 }
